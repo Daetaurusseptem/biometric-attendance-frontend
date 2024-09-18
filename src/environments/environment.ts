bd1d381ee0a5ff0865270d03e0bcc587b45b9baa
Jaime Antonio Burciaga Alvarez <78524937+Daetaurusseptem@users.noreply.github.com>
2024-09-17 18:13:54 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
     production: true,
-    //apiUrl: 'https://biometric-integration.onrender.com/api'
-    apiUrl: 'http://localhost:3000/api' // Para desarrollo local
-
+    apiUrl: 'https://biometric-integration.onrender.com/api'
   };
